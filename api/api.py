b85e4c4789cfca383a55b5f6048e967253a19c67
Mathew Buitendam <mathew.buitendam11@gmail.com>
2024-05-12 08:44:11 +1000

    working prototype of how to combine react and flask into one working PWA

--- api/api.py
+++ api/api.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/success')
+def success():
+    return "Just something to prove this is still active"
